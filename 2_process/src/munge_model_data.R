405a002f9e6a7cc6d2b2144595c28a9f7a8a90b8
Corson-Dosch <hcorson-dosch@usgs.gov>
2021-07-06 16:55:43 -0500

    Modify targets file to work w/ existing workflow and scripts

--- 2_process/src/munge_model_data.R
+++ 2_process/src/munge_model_data.R
@@ -4,7 +4,7 @@ library(readr)
 library(stringr)
 library(whisker)
 
-munge_rmses <- function(in_file, out_file) {
+munge_rmses <- function(in_file) {
   eval_data <- readr::read_csv(in_file, col_types = 'iccd') %>%
     filter(str_detect(exper_id, 'similar_[0-9]+')) %>%
     mutate(col = case_when(
@@ -17,22 +17,21 @@ munge_rmses <- function(in_file, out_file) {
       model_type == 'pgdl' ~ 23
     ), n_prof = as.numeric(str_extract(exper_id, '[0-9]+')))
   
-  readr::write_csv(eval_data, out_file)
+  return(eval_data)
 }
 
-render_model_diagnostics <- function(in_file, out_file) {
-  eval_data <- readr::read_csv(in_file, col_types = 'iccdcid')
+render_model_diagnostics <- function(data, out_file) {
   
   # Save the model diagnostics
-  render_data <- list(pgdl_980mean = filter(eval_data, model_type == 'pgdl', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
-                      dl_980mean = filter(eval_data, model_type == 'dl', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
-                      pb_980mean = filter(eval_data, model_type == 'pb', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
-                      dl_500mean = filter(eval_data, model_type == 'dl', exper_id == "similar_500") %>% pull(rmse) %>% mean %>% round(2),
-                      pb_500mean = filter(eval_data, model_type == 'pb', exper_id == "similar_500") %>% pull(rmse) %>% mean %>% round(2),
-                      dl_100mean = filter(eval_data, model_type == 'dl', exper_id == "similar_100") %>% pull(rmse) %>% mean %>% round(2),
-                      pb_100mean = filter(eval_data, model_type == 'pb', exper_id == "similar_100") %>% pull(rmse) %>% mean %>% round(2),
-                      pgdl_2mean = filter(eval_data, model_type == 'pgdl', exper_id == "similar_2") %>% pull(rmse) %>% mean %>% round(2),
-                      pb_2mean = filter(eval_data, model_type == 'pb', exper_id == "similar_2") %>% pull(rmse) %>% mean %>% round(2))
+  render_data <- list(pgdl_980mean = filter(data, model_type == 'pgdl', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
+                      dl_980mean = filter(data, model_type == 'dl', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_980mean = filter(data, model_type == 'pb', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
+                      dl_500mean = filter(data, model_type == 'dl', exper_id == "similar_500") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_500mean = filter(data, model_type == 'pb', exper_id == "similar_500") %>% pull(rmse) %>% mean %>% round(2),
+                      dl_100mean = filter(data, model_type == 'dl', exper_id == "similar_100") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_100mean = filter(data, model_type == 'pb', exper_id == "similar_100") %>% pull(rmse) %>% mean %>% round(2),
+                      pgdl_2mean = filter(data, model_type == 'pgdl', exper_id == "similar_2") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_2mean = filter(data, model_type == 'pb', exper_id == "similar_2") %>% pull(rmse) %>% mean %>% round(2))
   
   template_1 <- 'resulted in mean RMSEs (means calculated as average of RMSEs from the five dataset iterations) of {{pgdl_980mean}}, {{dl_980mean}}, and {{pb_980mean}}°C for the PGDL, DL, and PB models, respectively.
   The relative performance of DL vs PB depended on the amount of training data. The accuracy of Lake Mendota temperature predictions from the DL was better than PB when trained on 500 profiles 
@@ -40,4 +39,5 @@ render_model_diagnostics <- function(in_file, out_file) {
   The PGDL prediction accuracy was more robust compared to PB when only two profiles were provided for training ({{pgdl_2mean}} and {{pb_2mean}}°C, respectively). '
   
   whisker.render(template_1 %>% str_remove_all('\n') %>% str_replace_all('  ', ' '), render_data ) %>% cat(file = out_file)
+  return(out_file)
 }
--- _targets.R
+++ _targets.R
@@ -1,35 +1,45 @@
 library(targets)
-source("code.R")
+source('1_fetch/src/sb_utils.R')
+source('2_process/src/munge_model_data.R')
+source('3_visualize/src/plot_model_results.R')
 tar_option_set(packages = c("tidyverse", "stringr", "sbtools", "whisker"))
 
 list(
   # Get the data from ScienceBase
   tar_target(
     model_RMSEs_csv,
-    download_data(out_filepath = "model_RMSEs.csv"),
+    sb_download(sb_id = '5d925066e4b0c4f70d0d0599', 
+                file_name = 'me_RMSE.csv', 
+                out_file = '1_fetch/out/model_RMSEs.csv'),
     format = "file"
   ), 
   # Prepare the data for plotting
   tar_target(
     eval_data,
-    process_data(in_filepath = model_RMSEs_csv),
+    munge_rmses(in_file = model_RMSEs_csv),
   ),
   # Create a plot
   tar_target(
     figure_1_png,
-    make_plot(out_filepath = "figure_1.png", data = eval_data), 
+    plot_rmses(data = eval_data,
+               out_file = '3_visualize/out/figure_1.png'), 
     format = "file"
   ),
   # Save the processed data
   tar_target(
     model_summary_results_csv,
-    write_csv(eval_data, file = "model_summary_results.csv"), 
+    {
+      out_file = "2_process/out/model_summary_results.csv"
+      write_csv(eval_data, 
+                file = out_file)
+      return(out_file)
+    }, 
     format = "file"
   ),
   # Save the model diagnostics
   tar_target(
     model_diagnostic_text_txt,
-    generate_model_diagnostics(out_filepath = "model_diagnostic_text.txt", data = eval_data), 
+    render_model_diagnostics(data = eval_data, out_file = '2_process/out/model_diagnostic_text.txt'), 
     format = "file"
   )
 )
